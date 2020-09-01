d895a9dc7d51d2b69a2dc4bc719e0696e6387bfe
Saqoosha <a@saqoo.sh>
2020-09-01 19:26:16 +0900

    05: All cubes are managed by static methods

--- 05-adafruitnrf/app.ino
+++ 05-adafruitnrf/app.ino
@@ -4,11 +4,8 @@
 
 #include "cube.h"
 
-static std::shared_ptr<Cube> cubes[BLE_MAX_CONNECTION] = {nullptr};
-
 void setup() {
     Serial.begin(115200);
-    // while (!Serial) delay(10);
 
     Serial.println("Start...");
 
@@ -21,7 +18,7 @@ void setup() {
 
     Bluefruit.Scanner.setRxCallback(scan_callback);
     Bluefruit.Scanner.restartOnDisconnect(true);
-    Bluefruit.Scanner.filterUuid(Cube::SERVICE_UUID);
+    Bluefruit.Scanner.filterUuid(Cube::ServiceUUID);
     Bluefruit.Scanner.useActiveScan(true);
     Bluefruit.Scanner.start(0);
 
@@ -29,21 +26,12 @@ void setup() {
 }
 
 static int previousCall = 0;
-static int count = 0;
-static uint8_t lampData[3][7] = {
-    {0x03, 0x00, 0x01, 0x01, 0x00, 0xff, 0xff},
-    {0x03, 0x00, 0x01, 0x01, 0xff, 0x00, 0xff},
-    {0x03, 0x00, 0x01, 0x01, 0xff, 0xff, 0x00},
-};
 
 void loop() {
     int now = millis() / 1000;
     if (now - previousCall >= 2) {
         Serial.printf("now %d\n", now);
         previousCall = now;
-        if (++count == 3) {
-            count = 0;
-        }
     }
 }
 
@@ -60,21 +48,15 @@ void connect_callback(uint16_t conn_handle) {
     Serial.print("Connect Callback, conn_handle: ");
     Serial.println(conn_handle);
 
-    auto cube = std::make_shared<Cube>();
-    cube->setup(conn_handle);
-    cubes[conn_handle] = cube;
+    Cube::new_cube(conn_handle);
 
     delay(100);
-    // if (conn_handle < 3) {
     Bluefruit.Scanner.start(0);
-    // }
 }
 
 void disconnect_callback(uint16_t conn_handle, uint8_t reason) {
     Serial.printf("Disconnected, %d, reason = 0x%02X\n", conn_handle, reason);
-    Serial.printf("%p\n", cubes[conn_handle]);
-    cubes[conn_handle] = nullptr;
-    Serial.println("done.");
+    Cube::delete_cube(conn_handle);
 }
 
 /* Prints a hex list to the Serial Monitor */
--- 05-adafruitnrf/cube.h
+++ 05-adafruitnrf/cube.h
@@ -3,13 +3,39 @@
 #include <Arduino.h>
 #include <bluefruit.h>
 
+#include <memory>
+
 class Cube {
    public:
-    static uint8_t SERVICE_UUID[];
-    static uint8_t LAMP_CHARACTERISTIC_UUID[];
+    static uint8_t ServiceUUID[];
+    static uint8_t LampCharacteristicUUID[];
+    static uint8_t BatteryCharacteristicUUID[];
 
+    static std::shared_ptr<Cube> cubes[];
+
+    static void new_cube(uint16_t conn_handle) {
+        auto cube = std::make_shared<Cube>();
+        cube->setup(conn_handle);
+        Cube::cubes[conn_handle] = cube;
+    }
+
+    static void delete_cube(uint16_t conn_handle) {
+        Cube::cubes[conn_handle] = nullptr;
+    }
+
+   private:
+    static void notify_callback(BLEClientCharacteristic *chr, uint8_t *data, uint16_t len) {
+        Serial.printf("notify_callback: %d, %d\n", len, data[0]);
+        auto cube = Cube::cubes[chr->connHandle()];
+        cube->notify_battery(data[0]);
+    }
+
+   public:
     Cube()
-        : conn_handle(BLE_CONN_HANDLE_INVALID) {}
+        : conn_handle(BLE_CONN_HANDLE_INVALID),
+          service(nullptr),
+          lamp(nullptr),
+          battery(nullptr) {}
 
     ~Cube() {
         disconnect();
@@ -19,7 +45,7 @@ class Cube {
         this->conn_handle = conn_handle;
 
         Serial.print("Discovering toio Service ... ");
-        service = new BLEClientService(SERVICE_UUID);
+        service = std::make_shared<BLEClientService>(ServiceUUID);
         service->begin();
         if (!service->discover(conn_handle)) {
             Serial.println("No Service Found");
@@ -27,8 +53,9 @@ class Cube {
             return false;
         }
         Serial.println("Service Found");
+
         Serial.print("Discovering Lamp Characteristic ... ");
-        lamp = new BLEClientCharacteristic(LAMP_CHARACTERISTIC_UUID);
+        lamp = std::make_shared<BLEClientCharacteristic>(LampCharacteristicUUID);
         lamp->begin();
         if (!lamp->discover()) {
             Serial.println("No Characteristic Found. Characteristic is mandatory but not found. ");
@@ -36,8 +63,39 @@ class Cube {
             return false;
         }
         Serial.println("Characteristic Found");
+
+        Serial.print("Discovering Battery Characteristic ... ");
+        battery = std::make_shared<BLEClientCharacteristic>(BatteryCharacteristicUUID);
+        battery->begin();
+        if (!battery->discover()) {
+            Serial.println("No Characteristic Found. Characteristic is mandatory but not found. ");
+            disconnect();
+            return false;
+        }
+        battery->setNotifyCallback(&Cube::notify_callback);
+        battery->enableNotify();
+        Serial.println("Characteristic Found");
+
         setLamp();
         return true;
+    }
+
+    void notify_battery(uint8_t value) {
+        if (value <= 10) {
+            uint8_t data[] = {0x04, 0x00, 0x02,
+                              30, 1, 1, 255, 0, 0,
+                              30, 1, 1, 0, 0, 0};
+            lamp->write_resp(data, sizeof(data));
+        } else if (value <= 20) {
+            uint8_t data[] = {0x03, 0x00, 0x01, 0x01, 255, 0, 0};
+            lamp->write_resp(data, sizeof(data));
+        } else if (value <= 50) {
+            uint8_t data[] = {0x03, 0x00, 0x01, 0x01, 255, 176, 25};
+            lamp->write_resp(data, sizeof(data));
+        } else {
+            uint8_t data[] = {0x03, 0x00, 0x01, 0x01, 0, 255, 0};
+            lamp->write_resp(data, sizeof(data));
+        }
     }
 
     void disconnect() {
@@ -46,14 +104,9 @@ class Cube {
             Bluefruit.disconnect(conn_handle);
             conn_handle = BLE_CONN_HANDLE_INVALID;
         }
-        if (service) {
-            delete service;
-            service = nullptr;
-        }
-        if (lamp) {
-            delete lamp;
-            service = nullptr;
-        }
+        service = nullptr;
+        service = nullptr;
+        service = nullptr;
     }
 
     void setLamp() {
@@ -63,9 +116,13 @@ class Cube {
 
    private:
     uint16_t conn_handle;
-    BLEClientService *service;
-    BLEClientCharacteristic *lamp;
+    std::shared_ptr<BLEClientService> service;
+    std::shared_ptr<BLEClientCharacteristic> lamp;
+    std::shared_ptr<BLEClientCharacteristic> battery;
 };
 
-uint8_t Cube::SERVICE_UUID[] = {0xAE, 0xBB, 0xD7, 0xFC, 0x3E, 0xCF, 0x08, 0x95, 0x71, 0x45, 0x3B, 0x5B, 0x00, 0x01, 0xB2, 0x10};
-uint8_t Cube::LAMP_CHARACTERISTIC_UUID[] = {0xAE, 0xBB, 0xD7, 0xFC, 0x3E, 0xCF, 0x08, 0x95, 0x71, 0x45, 0x3B, 0x5B, 0x03, 0x01, 0xB2, 0x10};
+uint8_t Cube::ServiceUUID[] = {0xAE, 0xBB, 0xD7, 0xFC, 0x3E, 0xCF, 0x08, 0x95, 0x71, 0x45, 0x3B, 0x5B, 0x00, 0x01, 0xB2, 0x10};
+uint8_t Cube::LampCharacteristicUUID[] = {0xAE, 0xBB, 0xD7, 0xFC, 0x3E, 0xCF, 0x08, 0x95, 0x71, 0x45, 0x3B, 0x5B, 0x03, 0x01, 0xB2, 0x10};
+uint8_t Cube::BatteryCharacteristicUUID[] = {0xAE, 0xBB, 0xD7, 0xFC, 0x3E, 0xCF, 0x08, 0x95, 0x71, 0x45, 0x3B, 0x5B, 0x08, 0x01, 0xB2, 0x10};
+
+std::shared_ptr<Cube> Cube::cubes[BLE_MAX_CONNECTION];
