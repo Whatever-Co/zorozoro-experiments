f0bf4fbaaeccda3bf70bb21c8f1c30e0e40dfa33
Saqoosha <a@saqoo.sh>
2020-09-14 18:38:13 +0900

    11: Optimize destination selection

--- 11-simulator/Assets/Macb.cs
+++ 11-simulator/Assets/Macb.cs
@@ -60,26 +60,79 @@ public class Macb : MonoBehaviour
     }
 
 
+    class MoveInfo
+    {
+        public Transform cube;
+        public Vector3 target;
+        public float distance;
+    }
+
+
+    void Move(List<Vector3> target)
+    {
+        var info = cubes.Zip(target.OrderBy(a => System.Guid.NewGuid()), (c, t) =>
+        {
+            return new MoveInfo
+            {
+                cube = c.transform,
+                target = t,
+                distance = Vector3.Distance(c.transform.localPosition, t)
+            };
+        }).OrderBy(i => i.distance).ToList();
+        var totalDistance = info.Aggregate(0f, (t, i) => t += i.distance);
+        Debug.Log(totalDistance);
+        for (var j = 0; j < 100; j++)
+        // while (true)
+        {
+            for (var i = 0; i < cubes.Count / 2; i++)
+            {
+                var c0 = info[i];
+                var c1 = info[cubes.Count - i - 1];
+                var d0 = c0.distance + c1.distance;
+                var e0 = Vector3.Distance(c0.cube.transform.localPosition, c1.target);
+                var e1 = Vector3.Distance(c1.cube.transform.localPosition, c0.target);
+                var e2 = e0 + e1;
+                // Debug.Log($"d0={d0}, e0={e0}, e1={e1}, e2={e2}");
+                if (e2 < d0)
+                {
+                    var tmp = c0.target;
+                    c0.target = c1.target;
+                    c1.target = tmp;
+                    c0.distance = Vector3.Distance(c0.cube.transform.localPosition, c0.target);
+                    c1.distance = Vector3.Distance(c1.cube.transform.localPosition, c1.target);
+                }
+            }
+            var dist = info.Aggregate(0f, (t, i) => t += i.distance);
+            Debug.Log($"{j}, {dist}");
+            if (dist >= totalDistance)
+            {
+                break;
+            }
+            totalDistance = dist;
+            info = info.OrderBy(i => i.distance).ToList();
+        }
+
+        foreach (var i in info)
+        {
+            var c = i.cube;
+            var t = i.target;
+            var angle = Vector3.Angle(c.transform.TransformDirection(Vector3.forward), t - c.transform.localPosition);
+            var d = angle / 90f;
+            c.transform.DOLookAt(origin.TransformPoint(t), d).SetDelay(Random.value);
+            c.transform.DOLocalMove(t, i.distance / 0.1f)
+                .SetEase(Ease.Linear)
+                .SetDelay(d);
+        }
+    }
+
+
     int current = 0;
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            var target = targets[++current % targets.Count];
-            target = target.OrderBy(a => System.Guid.NewGuid()).ToList();
-            for (var i = 0; i < cubes.Count; i++)
-            {
-                var c = cubes[i];
-                var t = target[i];
-                var distance = Vector3.Distance(c.transform.localPosition, t);
-                var angle = Vector3.Angle(c.transform.TransformDirection(Vector3.forward), t - c.transform.localPosition);
-                var d = angle / 90f;
-                c.transform.DOLookAt(origin.TransformPoint(t), d).SetDelay(Random.value);
-                c.transform.DOLocalMove(t, distance / 0.1f)
-                    .SetEase(Ease.Linear)
-                    .SetDelay(d);
-            }
+            Move(targets[++current % targets.Count]);
         }
     }
 
